b5c0dde0c35bffefc75d087be3f12f5be75d5a94
jslipak <seriedigital@gmail.com>
2019-01-08 23:54:45 +0000

    Boot

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2019_01_05_182215) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    # t.index ["user_id"], name: "index_posts_on_user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
