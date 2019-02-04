205b78ee2b626568c5c36581f44914d4db3c0117
jslipak <seriedigital@gmail.com>
2019-02-04 23:28:59 +0000

    Implented enum for status into posts for approval workflow

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_05_182215) do
+ActiveRecord::Schema.define(version: 2019_02_04_230450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_01_05_182215) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
