259ba87c364d769369b0f6d7b0738b5ce33b1280
jslipak <seriedigital@gmail.com>
2019-03-11 23:37:25 +0000

    employee page

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,24 @@
-
 Rails.application.routes.draw do
-  resources :audit_logs, except: [:new, :edit, :destory]
-  
-  namespace :admin do
-    resources :users
-		resources :posts
-		resources :admin_users
+		  resources :audit_logs, except: [:new, :edit, :destory] do
+				      member do
+							        get :confirm
+									    end
+					    end
+		    
+		    namespace :admin do
+					    resources :users
+								resources :posts
+										resources :admin_users
 
-    root to: "users#index"
-  end
+										    root to: "users#index"
+											  end
 
-  resources :posts do
-    member do
-      get :approve
-    end
-  end
+			  resources :posts do
+					      member do
+								        get :approve
+										    end
+						    end
 
-  devise_for :users, skip: [:registrations]
-  root to: 'static#homepage'
+			    devise_for :users, skip: [:registrations]
+				  root to: 'static#homepage'
 end
